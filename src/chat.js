362985949e3f21ff734c1b7564cb2ad6eb58229d
nateps <nate@nateps.com>
2010-10-13 14:49:45 -0700

    Initial working version. The good idea here is that there are functions that return html given some state of data, and they also subscribe to events that rerender the html when the data changes. There is also a start at server syncing using socket.io and serializing custom data objects into JSON. The data objects are somewhat gross still.

---
+++ src/chat.js
@@ -0,0 +1,332 @@
+var clone = function(o) {
+  var F = function() {};
+  F.prototype = o;
+  return new F;
+}
+
+var isDefined = function(o) {
+  return typeof o !== 'undefined';
+}
+
+var isArray = function(o) {
+  return typeof o === 'object' && o.constructor === Array;
+}
+
+// Adapted from an example by Erik Karlsson
+// http://www.nonobtrusive.com/2009/07/24/custom-events-in-javascript-by-making-your-own-dispatcher-class/
+var dispatcher = (function() {
+  var events = [];
+  return {
+    addListener: function(event, callback) {
+      events[event] = events[event] || [];
+      events[event].push(callback);
+    },
+    removeListener: function(event, callback) {
+      if (events[event]) {
+        var i, listeners = events[event];
+        for (i = listeners.length; i--;) {
+          if (listeners[i] === callback) {
+            listeners.splice(i, 1);
+            return true;
+          }
+        }
+      }
+      return false;
+    },
+    fire: function(event, e) {
+      if (events[event]) {
+        var listeners = events[event],
+            len = listeners.length;
+        while (len--) {
+          listeners[len](e);
+        }
+      }
+    }
+  };
+})();
+
+var setPaths = function(start, parentPath) {
+  var i, prop, path;
+  for (i in start) {
+    prop = start[i];
+    if (prop._type != 'Ref') {
+      path = (parentPath) ? parentPath + '.' + i : i;
+      if (typeof prop.path == 'function') {
+        prop._path = path;
+      }
+      if (typeof prop == 'object') {
+        setPaths(prop, path);
+      }
+    }
+  }
+}
+var toTypeJSON = function() {
+  var init = {}, i, prop;
+  for (i in this) {
+    prop = this[i];
+    if (typeof prop !== 'function' && i != '_type' && i != '_path') {
+      init[i] = prop;
+    }
+  }
+  return {
+    __type: this._type,
+    __init: init
+  };
+}
+var getPath = function() {
+  if (!isDefined(this._path)) {
+    setPaths(world, '');
+  }
+  return this._path;
+}
+var Constructor = function(type, spec, proto) {
+  var f = (typeof spec == 'function') ? spec : function() {};
+  if (proto) {
+    f.prototype = (typeof proto == 'function') ? clone(proto.prototype) : proto;
+  }
+  f.prototype._type = type;
+  f.prototype.toJSON = toTypeJSON;
+  f.prototype.path = getPath;
+  return f;
+}
+
+var Val, List;
+(function() {
+  var funcs = ['pop', 'push', 'reverse', 'shift', 'sort', 'splice', 'unshift'],
+      func, i;
+  Val = new Constructor('Val',
+    function(init) {
+      this._value = (init && isDefined(init._value)) ? init._value : init;
+    }, {
+      _fireUpdate: function() {
+        var path = this.path();
+        dispatcher.fire('update:' + path);
+        socket.send(JSON.stringify([path, this]));
+      },
+      get: function() {
+        return this._value;
+      },
+      set: function(arg) {
+        this._value = arg;
+        this._fireUpdate();
+      },
+      setSilent: function(arg) {
+        this._value = arg;
+      }
+    });
+  List = new Constructor('List',
+    function(init) {
+      this._value = (init && isArray(init._value)) ? init._value : [];
+    }, Val);
+  // Expose functions that manipulate the contained array
+  for (i = 0; func = funcs[i++];) {
+    List.prototype[func] = (function() {
+      var f = Array.prototype[func];
+      return function() {
+        var out = f.apply(this._value, arguments);
+        this._fireUpdate();
+        return out;
+      };
+    })();
+  }
+})();
+this.Val = Val;
+this.List = List;
+
+function addElementUpdater(id, value, func) {
+  var el, eventName = 'update:' + value.path();
+  function callback() {
+    if (!el) el = document.getElementById(id);
+    try {
+      func(el);
+    } catch (e) {
+      dispatcher.removeListener(eventName, callback);
+    }
+  }
+  dispatcher.addListener(eventName, callback);
+}
+function addAttributeUpdater(id, attr, value) {
+  addElementUpdater(id, value, function(el) {
+    el.setAttribute(attr, value.get());
+  });
+}
+function addPropertyUpdater(id, prop, value) {
+  addElementUpdater(id, value, function(el) {
+    el[prop] = value.get();
+  });
+}
+function addInnerUpdater(id, value) {
+  addElementUpdater(id, value, function(el) {
+    el.innerHTML = value.get();
+  });
+}
+function addListUpdater(id, outItem, value) {
+  addElementUpdater(id, value, function(el) {
+    el.innerHTML = outList(outItem, value);
+  });
+}
+
+var uniqueId = (function() {
+  var id = 0;
+  return function() {
+    return '_' + (id++).toString(36);
+  }
+})();
+
+function outList(outItem, value) {
+  var items = value.get(),
+      html = '',
+      i, item;
+  for (i = 0; item = items[i++];) {
+    html += outItem(item);
+  }
+  return html;
+}
+
+var makeRef = function(root, init) {
+  var obj, props, prop, i, Ref = function() {}, ref;
+  props = init.split('.');
+  obj = root;
+  for (i = 0; prop = props[i++];) {
+    obj = obj[prop];
+  }
+  Ref.prototype = obj;
+  ref = new Ref;
+  ref._type = 'Ref';
+  ref.toJSON = function() {
+    return {
+      __ref: init
+    }
+  };
+  return ref;
+}
+// Takes a specially formatted JSON object and adds the values to the root
+var unpackJson = function(root, items) {
+  var key, val, i;
+  var makeObject = function(val) {
+    var obj, refs, ref, ii;
+    if (typeof val == 'object') {
+      if (isDefined(val.__type)) {
+        obj = (isDefined(val.__init)) ?
+          new this[val.__type](makeObject(val.__init)) :
+          new this[val.__type];
+      } else if (isDefined(val.__ref)) {
+        obj = makeRef(val.__ref, root);
+      } else {
+        obj = val;
+        for (ii in obj) {
+          obj[ii] = makeObject(obj[ii]);
+        }
+      }
+    } else {
+      obj = val;
+    }
+    return obj;
+  }
+  for (i = 0; key = items[i++], val = items[i++];) {
+    root[key] = makeObject(val);
+  }
+}
+
+var User = new Constructor('User',
+  function(init) {
+    this.name = init && init.name || new Val('');
+    this.picUrl = init && init.picUrl || new Val('');
+  });
+this.User = User;
+var Message = new Constructor('Message',
+  function(init) {
+    this.user = init && init.user || new User;
+    this.comment = init && init.comment || new Val('');
+  });
+this.Message = Message;
+
+// var json = [
+//   'users', [{
+//     __type: 'User',
+//     __init: {
+//       name: {
+//         __type: 'Val',
+//         __init: 'Nate'
+//       },
+//       picUrl: {
+//         __type: 'Val',
+//         __init: 'http://nateps.com/resume/nate_smith_92x92.jpg'
+//       }
+//     }
+//   }],
+//   'session', {
+//     user: {
+//       __ref: 'users.0'
+//     },
+//     newComment: {
+//       __type: 'Val',
+//       __init: ''
+//     }
+//   },
+//   'messages', {
+//     __type: 'List'
+//   }
+// ]
+// 
+// var world = {};
+// unpackJson(world, json);
+
+var outMessage = function(message) {
+  var picId = uniqueId(),
+      nameId = uniqueId(),
+      commentId = uniqueId();
+  addAttributeUpdater(picId, 'src', message.user.picUrl);
+  addInnerUpdater(nameId, message.user.name);
+  addInnerUpdater(commentId, message.comment);
+  
+  return '<li> \
+    <img id=' + picId + ' src="' + message.user.picUrl.get() + '" class=pic> \
+    <div class=message> \
+      <p><b id=' + nameId + '>' + message.user.name.get() + '</b> \
+      <p id=' + commentId + '>' + message.comment.get() + '</div>'
+}
+
+var outBody = function() {
+  var session = world.session,
+      messages = world.messages;
+  addListUpdater('messageList', outMessage, messages);
+  addAttributeUpdater('inputPic', 'src', session.user.picUrl);
+  addAttributeUpdater('inputName', 'value', session.user.name);
+  addPropertyUpdater('commentInput', 'value', session.newComment);
+  
+  return '<ul id=messageList>' + outList(outMessage, messages) + '</ul> \
+    <div id=foot> \
+      <img id=inputPic src="' + session.user.picUrl.get() + '" class=pic> \
+      <div id=inputs> \
+        <input id=inputName onkeyup=world.session.user.name.set(this.value) value="' + session.user.name.get() + '"> <b>(your nickname)</b> \
+        <form action=javascript:postMessage() style=position:relative;margin-top:6px;padding-right:6px> \
+          <input id=commentInput style=width:100% onkeyup=world.session.newComment.setSilent(this.value) value="' + session.newComment.get() + '"> \
+        </form> \
+      </div> \
+    </div>'
+}
+
+var postMessage = function() {
+  world.messages.push(
+    new Message({
+      user: makeRef(world, 'session.user'),
+      comment: new Val(world.session.newComment.get())
+    })
+  );
+  world.session.newComment.set('');
+}
+
+var world = {};
+this.world = world;
+world.users = [
+  new User({
+    name: new Val('Nate'),
+    picUrl: new Val('http://nateps.com/resume/nate_smith_92x92.jpg')
+  })
+];
+world.session = {
+  user: makeRef(world, 'users.0'),
+  newComment: new Val('')
+};
+world.messages = new List;
---
+++ src/server.js
@@ -0,0 +1,115 @@
+var HOST = null; // localhost
+var PORT = 8001;
+
+var http = require('http');
+var fs = require('fs');
+var sys = require('sys');
+var url = require('url');
+var qs = require('querystring');
+// var mongo = require('../lib/node-mongodb-native/lib/mongodb');
+var io = require('../lib/socket.io/lib/socket.io');
+var chat = require('./chat');
+
+// var mongoHost = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+// var mongoPort = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : mongo.Connection.DEFAULT_PORT;
+
+// console.log('Connecting to ' + mongoHost + ':' + mongoPort);
+// var db = new mongo.Db('vers', new mongo.Server(mongoHost, mongoPort, {}), {});
+// var dbItems;
+// db.open(function(err, db) {
+//   db.collection('items', function(err, collection) {
+//     dbItems = collection;
+//     collection.findOne({u:'sys'}, function(err, result) {
+//       for (var key in result) {
+//         client.setItem(key, result[key]);
+//       }
+//     });
+//   });
+// });
+// var updateItem = function(key, value) {
+//   if (dbItems) {
+//     client.setItem(key, value);
+//     var obj = {};
+//     obj[key] = value;
+//     dbItems.update({u:'sys'}, {$set:obj});
+//   }
+// };
+
+var getMap = {
+  '/': function(req, res) {
+    fs.readFile('chat2.html', 'utf8', function(err, data) {
+      res.htmlResponse(data);
+    });
+  },
+  '/extern.js': function(req, res) {
+    var externJs = '';
+    fs.readFile('chat.js', 'utf8', function(err, data) {
+      res.jsResponse(data);
+    });
+  },
+};
+var postMap = {
+  // '/set': function(req, res) {
+  //   var data = '';
+  //   req.addListener('data', function(chunk) {
+  //     data += chunk;
+  //   });
+  //   req.addListener('end', function() {
+  //     res.noContent();
+  //     var query = qs.parse(data);
+  //     for (var key in query) {
+  //       updateItem(key, query[key]);
+  //     }
+  //   })
+  // },
+};
+var requestMap = {GET:getMap, HEAD:getMap, POST:postMap};
+
+var textResponse = function(body, type, code) {
+  if (!code) code = 200;
+  this.writeHead(code, {
+    'Content-Length': body.length,
+    'Content-Type': type + '; charset=utf-8'
+  });
+  this.write(body, 'utf8');
+  this.end();
+};
+var notFound = function(req, res) {
+  textResponse.call(res, 'Not found\n', 'text/plain', 404);
+};
+http.ServerResponse.prototype.noContent = function() {
+  this.writeHead(204, {});
+  this.end();
+};
+http.ServerResponse.prototype.htmlResponse = function(body, code) {
+  textResponse.call(this, body, 'text/html', code);
+};
+http.ServerResponse.prototype.textResponse = function(body, code) {
+  textResponse.call(this, body, 'text/plain', code);
+};
+http.ServerResponse.prototype.manifestResponse = function(body, code) {
+  textResponse.call(this, body, 'text/cache-manifest', code);
+};
+http.ServerResponse.prototype.jsResponse = function(body, code) {
+  textResponse.call(this, body, 'application/javascript', code);
+};
+http.ServerResponse.prototype.jsonResponse = function(obj, code) {
+  var body = JSON.stringify(obj);
+  textResponse.call(this, body, 'application/json', code);
+};
+
+var server = http.createServer(function(req, res) {
+  var parsedUrl = url.parse(req.url);
+  var handler = requestMap[req.method][parsedUrl.pathname] || notFound;
+  handler(req, res);
+});
+
+var socket = io.listen(server);
+socket.on('connection', function(client) {      
+  client.on('message', function(message) {
+    console.log(message);
+  });
+});
+
+server.listen(PORT, HOST);
+console.log('Server at http://' + (HOST || '127.0.0.1') + ':' + PORT.toString() + '/');
